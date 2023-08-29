f35f87670375fa5f927b2aa3e6005ff09e770d38
“LiciaLicari” <licianbsplicari@gmail.com>
2023-08-29 19:20:54 +0200

    add Bonus Snack

--- SnackBonus/assets/js/script.js
+++ SnackBonus/assets/js/script.js
@@ -1,1 +1,22 @@
 console.log("Bonus snack");
+
+/*
+- Snack Bonus:
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+
+*/
+
+/*
+Tools:
+- prompt
+- const
+- let
+- .createElement
+- .querySelector
+- append
+- if/else
+- for
+- console.log
+*/
