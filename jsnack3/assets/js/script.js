32eb6e69a2d524beaca6ff0a4c11273f5960c556
“LiciaLicari” <licianbsplicari@gmail.com>
2023-08-29 16:50:31 +0200

    add snack3

--- jsnack3/assets/js/script.js
+++ jsnack3/assets/js/script.js
@@ -12,25 +12,39 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 
-Attenzione:
-Create una sotto-cartella per ogni jsnack e riportate la consegna all'inizio del file js.
-La struttura dei file e cartelle dovrebbe essere la seguente:
-:git: :cartella_file_aperta: js-jsnacks-blocco-1 (repo)
-      :cartella_file_aperta: jsnack1 (cartella)
-             index.html
-             script.js
-     :cartella_file_aperta:  jsnack2 (cartella)
-             index.html
-             script.js
-     :cartella_file_aperta:  jsnack3 (cartella)
-             index.html
-             script.js
-e cosi via.
-Consigli:
-se state scrivendo lo stesso codice identico o molto simile, considerate la possibilitá che serva un loop
-partite sempre con l'elenco degli strumenti
-scrivete in italiano cosa volete che il programma faccia (pseudo codice)
-Confermate lettura come al solito e buon divertimento! :baby-yoda:
-
-
 */
+
+/*
+Tools:
+- prompt
+- const
+- let
+- .createElement
+- .querySelector
+- append
+- if/else
+- for
+- console.log
+*/
+
+/* Defining variables and DOM elements*/
+
+const bodyElement = document.querySelector('body');
+const resultElement = document.createElement('div');
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+    const input = Number(prompt(`Insert number ${i+1}`));
+    sum += input;
+}
+
+console.log(sum);
+
+// print 
+bodyElement.append(resultElement);
+
+if (!isNaN(sum)) {
+    resultElement.append("Resulting sum is: " + sum);
+} else {
+    resultElement.append("Something went wrong");
+}
