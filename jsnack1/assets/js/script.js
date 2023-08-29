247fce5e7d8937b2ebbeb1fd385fa2c6961344fc
“LiciaLicari” <licianbsplicari@gmail.com>
2023-08-29 14:02:28 +0200

    Initial Commit

---
+++ jsnack1/assets/js/script.js
@@ -0,0 +1,31 @@
+console.log("Snack 1");
+
+/*
+
+Descrizione:
+- Snack 1:
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+
+Attenzione:
+Create una sotto-cartella per ogni jsnack e riportate la consegna all'inizio del file js.
+La struttura dei file e cartelle dovrebbe essere la seguente:
+:git: :cartella_file_aperta: js-jsnacks-blocco-1 (repo)
+      :cartella_file_aperta: jsnack1 (cartella)
+             index.html
+             script.js
+     :cartella_file_aperta:  jsnack2 (cartella)
+             index.html
+             script.js
+     :cartella_file_aperta:  jsnack3 (cartella)
+             index.html
+             script.js
+e cosi via.
+Consigli:
+se state scrivendo lo stesso codice identico o molto simile, considerate la possibilitá che serva un loop
+partite sempre con l'elenco degli strumenti
+scrivete in italiano cosa volete che il programma faccia (pseudo codice)
+Confermate lettura come al solito e buon divertimento! :baby-yoda:
+
+
+*/
---
+++ jsnack2/assets/js/script.js
@@ -0,0 +1,32 @@
+console.log("Snack 2");
+
+/*
+
+Descrizione:
+
+- Snack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+
+Attenzione:
+Create una sotto-cartella per ogni jsnack e riportate la consegna all'inizio del file js.
+La struttura dei file e cartelle dovrebbe essere la seguente:
+:git: :cartella_file_aperta: js-jsnacks-blocco-1 (repo)
+      :cartella_file_aperta: jsnack1 (cartella)
+             index.html
+             script.js
+     :cartella_file_aperta:  jsnack2 (cartella)
+             index.html
+             script.js
+     :cartella_file_aperta:  jsnack3 (cartella)
+             index.html
+             script.js
+e cosi via.
+Consigli:
+se state scrivendo lo stesso codice identico o molto simile, considerate la possibilitá che serva un loop
+partite sempre con l'elenco degli strumenti
+scrivete in italiano cosa volete che il programma faccia (pseudo codice)
+Confermate lettura come al solito e buon divertimento! :baby-yoda:
+
+
+*/
